6f482c14d999e92b64e8713a3beea759c8f0326b
Christopher Githu <christopher.githu@gmail.com>
2018-02-14 14:11:01 +0300

    DROP INDEX IX_mst_Patient ON mst_Patient

--- Migration/DB Prep/Scripts/TableStructure/003 Indexes.sql
+++ Migration/DB Prep/Scripts/TableStructure/003 Indexes.sql
@@ -472,3 +472,7 @@ INCLUDE (
 	[ParameterId],
 	[ResultValue]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
+
+IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[mst_Patient]') AND name = N'IX_mst_Patient')
+DROP INDEX [IX_mst_Patient] ON [dbo].[mst_Patient] WITH ( ONLINE = OFF )
+GO
