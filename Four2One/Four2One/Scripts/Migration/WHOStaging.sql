c505aa0a43d7c956557c964fa2194f7afef42ed9
chris-githu <christopher.githu@gmail.com>
2018-09-17 11:23:09 +0300

    OIs

--- Four2One/Four2One/Scripts/Migration/WHOStaging.sql
+++ Four2One/Four2One/Scripts/Migration/WHOStaging.sql
@@ -39,6 +39,7 @@ and CASE a.WHOStage WHEN 1 THEN 132
 	WHEN 4 THEN 135
 ELSE NULL END IS NOT NULL
 GO
+
 /*
 135	WHOStage	Stage4
 134	WHOStage	Stage3
@@ -46,14 +47,11 @@ GO
 132	WHOStage	Stage1
 */
 
-/*
 WITH OIs1 AS (SELECT a.Ptn_Pk
 , a.Visit_Id
 , CASE WHEN COALESCE(d3Blue.Name, d3.Name) LIKE '%none%' OR COALESCE(d3Blue.Name, d3.Name) LIKE '%Not Documented%' THEN NULL
 	WHEN Tb.Ptn_pk is not Null Then 'Pulmonary TB'
 ELSE COALESCE(d3Blue.Name, d3.Name) END AS OI
-
-
 FROM ord_Visit a 
 
 INNER JOIN dtl_PatientDisease e ON a.Visit_Id = e.Visit_Pk 
@@ -77,7 +75,22 @@ and (c.DeleteFlag = 0 or c.DeleteFlag is null))
 , OIs AS
 (SELECT a.Ptn_Pk
 , cast(b.visitdate as date) VisitDate
-, CASE WHEN OI LIKE '%pulmonary%tb%' THEN 'Pulmonary TB' ELSE a.OI END AS OI
+, CASE WHEN OI LIKE '%pulmonary%tb%' THEN 'Pulmonary TB' 
+WHEN OI LIKE '%Oral%Candidiasis%' OR OI LIKE 'Thrush(oral/vaginal)' THEN 'Oral candidiasis thrush'
+WHEN OI LIKE '%Weight%loss%' THEN 'Weight loss > 10%'
+WHEN OI LIKE '%KS%Cutaneous%' THEN 'KS Cutaneous'
+WHEN OI LIKE '%Esophageal%Candidiasis%' THEN 'Candidiasis Oesephogeal'
+WHEN OI LIKE '%KS%Visceral%' THEN 'KS Visceral'
+WHEN OI LIKE '%Cryptococcal%Meningitis%' THEN 'Extrapulmonary Cryptococcosis'
+WHEN OI LIKE '%Zoster%' THEN 'Herpes zoster'
+WHEN OI LIKE '%Genital%Ulcerative%Disease%' THEN 'Ulcerative gingivitis'
+WHEN OI LIKE '%Herpes%Simplex%' THEN 'Chronic Herpes simplex'
+WHEN OI LIKE '%DEmentia%' THEN 'Encephalopathy dementia'
+WHEN OI LIKE '%Chronic%Diarrhoea%Wasting%' THEN 'Chronic diarrhoea wasting'
+WHEN OI LIKE '%Other%Mycobacteria%' THEN 'Mycobacteria'
+
+
+ELSE a.OI END AS OI
 FROM (
 Select * FROM OIs1 WHERE OI IS NOT NULL 
 UNION
@@ -86,17 +99,21 @@ INNER JOIN ord_Visit b ON a.Visit_Id = b.Visit_Id
 WHERE OI NOT IN ('Other','COUGH') AND 
 (b.DeleteFlag = 0 or b.DeleteFlag is null))
 
+INSERT INTO PatientOI
+(PatientId, PatientMasterVisitId, OIId, DeleteFlag, CreatedBy, CreateDate)
 SELECT 
-
-/*a.Ptn_Pk
-, a.OI
-, c.*
-*/
-a.OI, COUNT(a.Ptn_Pk)n
-
+distinct b.Id PatientId
+, c.Id PatientMasterVisitId
+, l.ItemId OIId
+, 0 DeleteFlag
+, 1 CreatedBy
+, c.CreateDate
  FROM OIs a 
-	left join LookupItemView c on a.OI = c.DisplayName and c.MasterName LIKE 'WHOStage%Conditions'
-WHERE c.ItemDisplayName is null
-GROUP BY a.OI 
-ORDER BY n
-*/
+inner join LookupItemView l on a.OI = l.DisplayName and l.MasterName LIKE 'WHOStage%Conditions'
+inner join Patient b on a.Ptn_pk = b.ptn_pk
+inner join PatientMasterVisit c on b.id = c.PatientId
+and cast(a.visitdate as date) = cast(c.visitdate as date)
+and c.VisitType = 0
+left join PatientOI d ON c.Id = d.PatientMasterVisitId
+where d.PatientMasterVisitId is null
+GO
